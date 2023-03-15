980153946c144d936768638434bc3a0d30cf5e5b
Ramil Shaybakov <rshaibakov@gmail.com>
2023-03-15 16:28:42 +0500

    ci(test): debug env

--- src/db.ts
+++ src/db.ts
@@ -1,5 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
+console.log('VITE_SUPABASE_URL', import.meta.env.VITE_SUPABASE_URL)
 export const supabase = createClient(
   import.meta.env.VITE_SUPABASE_URL,
   import.meta.env.VITE_SUPABASE_KEY
