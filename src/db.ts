b116d5100ad949c229527ca2c59d6781b9a2d7d5
Ramil Shaybakov <rshaibakov@gmail.com>
2023-03-26 16:01:15 +0500

    ci(test): delete console.log

--- src/db.ts
+++ src/db.ts
@@ -1,6 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
 
-console.log('VITE_SUPABASE_URL', import.meta.env.VITE_SUPABASE_URL)
 export const supabase = createClient(
   import.meta.env.VITE_SUPABASE_URL,
   import.meta.env.VITE_SUPABASE_KEY
