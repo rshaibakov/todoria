1187b093ad7870cdf383d1b8c439f5b046685095
Ramil Shaybakov <rshaibakov@gmail.com>
2023-05-08 17:03:27 +0500

    fix(currentSprint): remove old list

--- src/features/CurrentSprint/CurrentSprint.vue
+++ src/features/CurrentSprint/CurrentSprint.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed, onBeforeMount, onMounted } from 'vue'
+import { computed, onBeforeMount } from 'vue'
 import { storeToRefs } from 'pinia'
 import dayjs from 'dayjs'
 import { useSprintsStore } from '../../stores/sprints'
@@ -27,58 +27,16 @@ const days = computed(() => {
 onBeforeMount(() => {
   store.fetchCurrentSprint()
 })
-
-onMounted(() => {
-  const scrollContainer = document.querySelector('[data-js-id=current-sprint-days]')
-
-  if (scrollContainer) {
-    scrollContainer.addEventListener('wheel', (event) => {
-      event.preventDefault()
-      scrollContainer.scrollLeft += (<WheelEvent>event).deltaY
-    })
-  }
-})
 </script>
 
 <template>
-  <section>
+  <section class="current-sprint">
     <header>{{ currentSprint?.number }}</header>
-
-    <ul
-      class="days"
-      data-js-id="current-sprint-days"
-    >
-      <li
-        v-for="day in days"
-        :key="day"
-        data-test-id="current-sprint-day"
-        class="day"
-      >
-        {{ day }}
-      </li>
-    </ul>
   </section>
 </template>
 
 <style scoped>
-.days {
-  @apply
-    py-4
-    flex
-    flex-nowrap
-    gap-x-4
-    overflow-x-hidden;
-
-  height: 500px;
-}
-
-.day {
-  @apply
-    px-4
-    py-2
-    rounded-md
-    shadow-md;
-
-  min-width: 14%;
+.current-sprint {
+  position: relative;
 }
 </style>
