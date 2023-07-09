348031b3f329974a616ece1fb568d721d1d22717
Ramil Shaybakov <rshaibakov@gmail.com>
2023-07-09 14:04:39 +0500

    feat(task): add updated task revert

--- src/stores/tasks.ts
+++ src/stores/tasks.ts
@@ -3,18 +3,17 @@ import { ref } from 'vue'
 import { supabase } from '../db'
 import { type Database } from '../db.types'
 import { useUserStore } from './user'
-import { useSprintsStore } from './sprints'
+import { useSprintsStore, type TSprint } from './sprints'
 
 export type TTask = Database['public']['Tables']['tasks']['Row']
-export type TCreatedTask = Pick<Database['public']['Tables']['tasks']['Insert'], 'name' | 'description' | 'planned_at' | 'is_done' | 'sprint_id'>
-export type TUpdatedTask = Database['public']['Tables']['tasks']['Update']
-export type TSprintId = Database['public']['Tables']['sprints']['Row']['id']
+export type TTaskPayloadByCreate = Pick<Database['public']['Tables']['tasks']['Insert'], 'name' | 'description' | 'planned_at' | 'is_done' | 'sprint_id'>
+export type TTaskPayloadByUpdate = Database['public']['Tables']['tasks']['Update']
 
 // TODO: Добавить обработку ошибок
 export const useTasksStore = defineStore('tasks', () => {
   const tasks = ref<TTask[]>([])
 
-  const fetchTasksBySprint = async (sprintId: TSprintId) => {
+  const fetchTasksBySprint = async (sprintId: TSprint['id']) => {
     const { data, error } = await supabase.from('tasks')
       .select()
       .eq('sprint_id', sprintId)
@@ -30,7 +29,7 @@ export const useTasksStore = defineStore('tasks', () => {
 
   const userStore = useUserStore()
 
-  const createTask = async (task: TCreatedTask) => {
+  const createTask = async (task: TTaskPayloadByCreate) => {
     if (userStore.user === null) {
       throw Error('User not found')
     }
@@ -55,6 +54,49 @@ export const useTasksStore = defineStore('tasks', () => {
 
   const sprintStore = useSprintsStore()
 
+  const createTaskByCurrentSprint = async (task: TTaskPayloadByCreate) => {
+    if (sprintStore.currentSprint === null) {
+      throw Error('Sprint not found')
+    }
+
+    const newTask = await createTask({
+      ...task,
+      sprint_id: sprintStore.currentSprint.id
+    })
+
+    tasks.value = [newTask, ...tasks.value]
+
+    if (newTask.planned_at !== null) {
+      tasks.value = tasks.value.sort(sortTasks)
+    }
+  }
+
+  // TODO: Требует рефакторинга
+  const updateTask = async (taskId: TTaskPayloadByUpdate['id'], taskPayload: TTaskPayloadByUpdate) => {
+    const currentTaskIndex = tasks.value.findIndex(task => task.id === taskId)
+    const currentTask = tasks.value[currentTaskIndex]
+
+    tasks.value.splice(currentTaskIndex, 1, { ...currentTask, ...taskPayload })
+
+    if (taskPayload.planned_at !== currentTask.planned_at) {
+      tasks.value = tasks.value.sort(sortTasks)
+    }
+
+    const { error } = await supabase.from('tasks')
+      .update(taskPayload)
+      .eq('id', taskId)
+
+    if (error !== null) {
+      tasks.value.splice(currentTaskIndex, 1, currentTask)
+
+      if (taskPayload.planned_at !== currentTask.planned_at) {
+        tasks.value = tasks.value.sort(sortTasks)
+      }
+
+      throw Error(error.message)
+    }
+  }
+
   const sortTasks = (a: TTask, b: TTask) => {
     if (a.planned_at !== null && b.planned_at !== null) {
       return a.planned_at > b.planned_at ? 1 : -1
@@ -69,36 +111,6 @@ export const useTasksStore = defineStore('tasks', () => {
     }
 
     return 0
-  }
-
-  const createTaskByCurrentSprint = async (task: TCreatedTask) => {
-    if (sprintStore.currentSprint === null) {
-      throw Error('Sprint not found')
-    }
-
-    const newTask = await createTask({
-      ...task,
-      sprint_id: sprintStore.currentSprint.id
-    })
-
-    if (newTask.planned_at !== null) {
-      tasks.value = [newTask, ...tasks.value].sort(sortTasks)
-    } else {
-      tasks.value = [newTask, ...tasks.value]
-    }
-  }
-
-  const updateTask = async (taskId: TUpdatedTask['id'], task: TUpdatedTask) => {
-    const updatedTaskIndex = tasks.value.findIndex(task => task.id === taskId)
-    tasks.value.splice(updatedTaskIndex, 1, { ...tasks.value[updatedTaskIndex], ...task })
-
-    const { error } = await supabase.from('tasks')
-      .update(task)
-      .eq('id', taskId)
-
-    if (error !== null) {
-      throw Error(error.message)
-    }
   }
 
   return {
