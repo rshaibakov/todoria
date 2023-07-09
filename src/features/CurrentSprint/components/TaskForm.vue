2456f2156cc2bb941985cf146b09a3718e8871e5
Ramil Shaybakov <rshaibakov@gmail.com>
2023-07-09 15:05:34 +0500

    feat(taskForm): add remove button

--- src/features/CurrentSprint/components/TaskForm.vue
+++ src/features/CurrentSprint/components/TaskForm.vue
@@ -1,11 +1,10 @@
 <script setup lang="ts">
-import { computed, ref } from 'vue'
+import { ref } from 'vue'
 import dayjs from 'dayjs'
 import { TTask, useTasksStore } from '../../../stores/tasks'
 
 const props = defineProps<{ task?: TTask }>()
-
-const emit = defineEmits(['cancel', 'submit'])
+const emit = defineEmits(['cancel', 'remove', 'submit'])
 
 const { createTaskByCurrentSprint, updateTask } = useTasksStore()
 
@@ -13,7 +12,10 @@ const nameField = ref<HTMLInputElement | null>(null)
 const descriptionField = ref<HTMLTextAreaElement | null>(null)
 const dateField = ref<HTMLInputElement | null>(null)
 
-const taskPlannedAt = computed(() => props.task?.planned_at && dayjs(props.task?.planned_at).format('YYYY-MM-DD'))
+const isForUpdate = props.task !== undefined
+const taskPlannedAt = isForUpdate
+  ? props.task.planned_at && dayjs(props.task?.planned_at).format('YYYY-MM-DD')
+  : ''
 
 const onSubmit = async () => {
   if (!nameField.value) {
@@ -22,7 +24,7 @@ const onSubmit = async () => {
 
   emit('submit')
 
-  if (props.task !== undefined) {
+  if (isForUpdate) {
     await updateTask(props.task.id, {
       name: nameField.value.value,
       description: descriptionField.value?.value,
@@ -64,13 +66,23 @@ const onSubmit = async () => {
       ref="dateField"
       class="text-field"
       data-test-id="task-form-planned-at-field"
-      :value="taskPlannedAt || ''"
+      :value="taskPlannedAt"
       type="date"
     >
 
     <footer class="actions">
       <button
-        class="button button-sm"
+        v-if="isForUpdate"
+        class="button button-sm button-danger remove-button"
+        data-test-id="task-form-remove-button"
+        type="button"
+        @click.stop="emit('remove')"
+      >
+        Удалить
+      </button>
+
+      <button
+        class="button button-sm cancel-button"
         data-test-id="task-form-cancel-button"
         type="button"
         @click.stop="emit('cancel')"
@@ -79,11 +91,11 @@ const onSubmit = async () => {
       </button>
 
       <button
-        class="button button-sm button-primary"
+        class="button button-sm button-primary save-button"
         data-test-id="task-form-submit-button"
         type="submit"
       >
-        Добавить
+        {{ isForUpdate ? 'Обновить' : 'Добавить' }}
       </button>
     </footer>
   </form>
@@ -102,7 +114,20 @@ const onSubmit = async () => {
 
 .actions {
   @apply
-    flex
-    justify-end;
+    grid;
+
+  grid-template-columns: [one] 1fr [two] 1fr [three] 1fr [four] 1fr [five];
+}
+
+.remove-button {
+  grid-column: one / span two;
+}
+
+.cancel-button {
+  grid-column: three / span four;
+}
+
+.save-button {
+  grid-column: four / span five;
 }
 </style>
